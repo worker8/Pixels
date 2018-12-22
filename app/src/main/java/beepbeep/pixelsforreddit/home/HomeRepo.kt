cebf2a6b48f40d0448949a0389864a6503b070ba
Tan Jun Rong <tanjunrong@gmail.com>
2018-12-22 13:22:43 +0900

    remove jraw reference

--- app/src/main/java/beepbeep/pixelsforreddit/home/HomeRepo.kt
+++ app/src/main/java/beepbeep/pixelsforreddit/home/HomeRepo.kt
@@ -1,11 +1,11 @@
 package beepbeep.pixelsforreddit.home
 
-import com.worker8.redditapi.RedditApi2
+import com.worker8.redditapi.RedditApi
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 
 class HomeRepo2(subreddit: String = "pics") {
-    val redditApi = RedditApi2()
+    val redditApi = RedditApi()
     fun getPosts() = redditApi.getPosts()
     fun getMainThread() = AndroidSchedulers.mainThread()
     fun getBackgroundThread() = Schedulers.io()
--- app/src/main/java/beepbeep/pixelsforreddit/home/HomeViewHolder.kt
+++ app/src/main/java/beepbeep/pixelsforreddit/home/HomeViewHolder.kt
@@ -2,33 +2,33 @@ package beepbeep.pixelsforreddit.home
 
 import android.graphics.drawable.ColorDrawable
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import beepbeep.pixelsforreddit.R
-import beepbeep.pixelsforreddit.imgur_api.model.GalleryImage
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.worker8.redditapi.RedditLink
 import kotlinx.android.synthetic.main.home_item.view.*
 
 
-class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    fun bind(galleryImage: GalleryImage) {
+class HomeViewHolder2(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(redditLink: RedditLink) {
         itemView.apply {
             context?.also { _context ->
 
                 val cropOptions = RequestOptions()
                         .fitCenter()
                         .placeholder(ColorDrawable(0xaaAAaa))
-
+                Log.d("ddw", "redditLink.url: ${redditLink.value.url}")
                 Glide.with(_context)
-                        .load(galleryImage.link)
+                        .load(redditLink.value.url)
                         .apply(cropOptions)
                         .into(homeItemImage)
-
             }
 
-            homeItemTitle.text = galleryImage.title
+            homeItemTitle.text = redditLink.value.title
         }
     }
 
@@ -36,6 +36,6 @@ class HomeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun create(parent: ViewGroup) =
                 LayoutInflater.from(parent.context)
                         .inflate(R.layout.home_item, parent, false)
-                        .let { HomeViewHolder(it) }
+                        .let { HomeViewHolder2(it) }
     }
 }
