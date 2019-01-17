b305fd6cf973a5e4a9e23238f32d0a0bd0438f79
Tan Jun Rong <tanjunrong@gmail.com>
2019-01-18 08:42:52 +0900

    up version again, to make versioning correct

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -2,6 +2,6 @@ class AndroidConfig {
     val sdkVersion = 28 // target & compile sdk
     val minSdkVersion = 21
     val targetSdkVersion = 28
-    val versionCode = 3
-    val versionName = "1.1.2"
+    val versionCode = 4
+    val versionName = "1.1.3"
 }
