f1552dfb995484764385d3bd765ab04a262290fa
Tan Jun Rong <tanjunrong@gmail.com>
2019-02-04 00:25:21 +0900

    up version

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -2,6 +2,6 @@ class AndroidConfig {
     val sdkVersion = 28 // target & compile sdk
     val minSdkVersion = 21
     val targetSdkVersion = 28
-    val versionCode = 5
-    val versionName = "1.1.4"
+    val versionCode = 6
+    val versionName = "1.1.5"
 }
